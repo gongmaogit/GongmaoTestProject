f9bc67b0b9434d3e0bb8f998c8d3bad82f880b09
gongmao <gomgmai@qq.com>
2017-12-09 12:26:30 +0800

    add in branch1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld in branch1");
+        System.out.println("HelloWorld in branch1");
     }
 }
